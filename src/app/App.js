7ef832a7e2d97f28793c40a2dfd797f011be996d
WeijiaSun <iloveusomuch@me.com>
2019-11-14 20:32:08 -0600

    the jeff basil

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+import Chase from './components/chase'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Chase />
+      </header>
+    </div>
+  );
+}
+
+export default App;
